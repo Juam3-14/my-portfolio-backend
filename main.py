05d1b46167d61a5d78f95754d98c1402763e67af
Juan Pablo Piemonte <jppiemonte@gmail.com>
2024-09-03 22:35:08 -0300

    ADD: Primera versión de RAG

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
-
 from llama_index.llms.openai import OpenAI
-from llama_index.agent.openai import OpenAIAgent
-
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, Settings
-from llama_index.llms.ollama import Ollama
-
-from llama_index.core import VectorStoreIndex
-from llama_index.vector_stores.chroma import ChromaVectorStore
-import chromadb, os
-from chromadb.utils import embedding_functions
-
-#model = "gpt-3.5-turbo"
-#prompt="Eres un pirata."
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 
-print("Holaaa")
+import os
 
-Settings.llm = Ollama(model="llama2", request_timeout=60.0)
-#--embeddingfunction = embedding_functions.OpenAIEmbeddingFunction(model_name="text_embedding-ada-002", api_key=apiKey)
-#--chroma_client=chromadb.HttpClient(host='localhost', port=8000)
+from getpass import getpass
 
-documents = SimpleDirectoryReader(input_dir='./app/resources').load_data()
-db = chromadb.PersistentClient(path="./chroma_db")
-chroma_collection = db.get_or_create_collection("quickstart")
-vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
+apikey = os.environ["OPENAI_API_KEY"]
 
-index = VectorStoreIndex.from_documents(
-    documents, storage_context=storage_context
-)
+documents = SimpleDirectoryReader('app/resources').load_data()
 
+index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
-response = query_engine.query("De que se trata el texto?")
+
+
+response = query_engine.query("de qué se trata el cuento?")
+print(response)
